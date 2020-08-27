6876078319cecd42682ce0727435bdaef534e879
davemgj84 <davemgj@gmail.com>
2020-08-27 01:29:54 +0000

    Completed challenge with Nicole and David R. - Wrote 2 extra Tests

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// PAIR PROGRAMMING WITH NICOLE AND DAVID R :)
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+  let result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (result[j] === undefined) {
+        result[j] = [];
+      }
+      result[j][i] = matrix[i][j];
+    }
+  }
+  return result;
+};
+
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  for (let l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = wordSearch;
